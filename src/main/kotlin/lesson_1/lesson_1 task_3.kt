39d500212ae64544191c1469831dc90fc7f23fec
Muhammad1977-RTS <m22850616@gmail.com>
2023-11-25 20:26:14 +0300

    KS-1-3 Gagarin_space

---
+++ src/main/kotlin/lesson_1/lesson_1 task_3.kt
@@ -0,0 +1,18 @@
+package lesson_1
+
+fun main() {
+    val yearOfGagarinFlight = "1961"
+    var hourOfFlight = "взлёт в 09:"
+    var minuteOfFlight = ":07"
+
+    println(yearOfGagarinFlight)
+    println(hourOfFlight)
+    println(minuteOfFlight)
+
+    hourOfFlight = "посадка в 10:"
+    minuteOfFlight = "55"
+
+    print(hourOfFlight)
+    println(minuteOfFlight)
+
+}
